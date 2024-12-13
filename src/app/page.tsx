8504590f1eaefb6a599cc89d8e222d149846aef9
whiffy <bryanzhao168@yahoo.ca>
2024-12-13 11:53:53 -0800

    t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "如何成为一个好老师",
+  description: "成为好老师的技巧",
+};
+
 export default function Home() {
   return <div>CHIN341</div>;
 }

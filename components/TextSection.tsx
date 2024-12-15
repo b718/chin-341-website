91055f47ecea9aa34599956698e69b7d7b4b53a8
whiffy <bryanzhao168@yahoo.ca>
2024-12-15 01:12:09 -0800

    Added uppercase styling for the headers.

--- components/TextSection.tsx
+++ components/TextSection.tsx
@@ -8,7 +8,9 @@ type TextSection = {
 const TextSection: FunctionComponent<TextSection> = ({ title, content }) => {
   return (
     <div style={{ marginLeft: "2rem", marginRight: "2rem" }}>
-      <h1 style={{ marginTop: "2rem" }}>{title}</h1>
+      <h1 style={{ marginTop: "2rem", textTransform: "capitalize" }}>
+        {title}
+      </h1>
       <div style={{ marginTop: "1rem", textAlign: "justify" }}>{content}</div>
     </div>
   );
